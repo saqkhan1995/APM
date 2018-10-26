631693fe8d6cf4c3a485a4898f7780cbcfa907aa
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2018-10-26 19:08:07 +0530

    UsingInterface

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
@@ -10,7 +11,8 @@ export class ProductListComponent{
     imageWidth: number = 50;
     imageMargin: 2;
     showImage: boolean = false;
-    products: any[] = [
+    listFilter: string = 'cart';
+    products: IProduct[] = [
         
             {
               "productId": 1,
