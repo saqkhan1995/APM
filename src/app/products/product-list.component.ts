5026914925a7164e019149b6c35d64f89bb20af8
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2018-10-28 15:56:03 +0530

    AddedstyleURLs
    AddedstyleURLs

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent{
@@ -10,7 +12,8 @@ export class ProductListComponent{
     imageWidth: number = 50;
     imageMargin: 2;
     showImage: boolean = false;
-    products: any[] = [
+    listFilter: string = 'cart';
+    products: IProduct[] = [
         
             {
               "productId": 1,
