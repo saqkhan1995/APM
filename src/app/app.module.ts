d0d547614fdc4bf6854e72e39939c669d69fc24d
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2018-10-29 17:04:08 +0530

    UsingNesterComponent
    UsingNesterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
