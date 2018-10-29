4d0ecc0de47aa261dbbde8668996bae3d9819822
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2018-10-29 18:54:11 +0530

    NestingComplete
    NestingComplete

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from "@angular/core";
+import { Component, OnChanges, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector:'pm-star',
@@ -7,11 +7,15 @@ import { Component, OnChanges } from "@angular/core";
 
 })
 export class StarComponent implements OnChanges{
-    rating:number = 4;
+    @Input() rating: number;
     starWidth: number;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
     ngOnChanges():void {
-        this.starWidth = this.rating * 75/5;
+        this.starWidth = this.rating * 75 / 5;
     }
 
+    onClick(): void {
+        this.ratingClicked.emit(`the rating ${this.rating} was clicked !`);
+    }
 }
