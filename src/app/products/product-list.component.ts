d4314ecc822ae3f361d70699b792ce31dd1a13b2
Saquib Khan <35995394+saqkhan1995@users.noreply.github.com>
2018-10-30 19:02:35 +0530

    injectingTheService
    injectingTheService

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -7,7 +8,7 @@ import { IProduct } from './product';
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string ='Product List';
     imageWidth: number = 50;
     imageMargin: 2;
@@ -22,64 +23,10 @@ export class ProductListComponent{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        
-            {
-              "productId": 1,
-              "productName": "Leaf Rake",
-              "productCode": "GDN-0011",
-              "releaseDate": "March 19, 2016",
-              "description": "Leaf rake with 48-inch wooden handle.",
-              "price": 19.95,
-              "starRating": 3.2,
-              "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/412AAGIyssL.jpg"
-            },
-            {
-              "productId": 2,
-              "productName": "Garden Cart",
-              "productCode": "GDN-0023",
-              "releaseDate": "March 18, 2016",
-              "description": "15 gallon capacity rolling garden cart",
-              "price": 32.99,
-              "starRating": 4.2,
-              "imageUrl": "https://www.steelfort.co.nz/assets/product-images/Cub-Cadet/GardenCart.png"
-            },
-            {
-              "productId": 5,
-              "productName": "Hammer",
-              "productCode": "TBX-0048",
-              "releaseDate": "May 21, 2016",
-              "description": "Curved claw steel hammer",
-              "price": 8.9,
-              "starRating": 4.8,
-              "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/61EJmWzoZmL._SL1500_.jpg"
-            },
-            {
-              "productId": 8,
-              "productName": "Saw",
-              "productCode": "TBX-0022",
-              "releaseDate": "May 15, 2016",
-              "description": "15-inch steel blade hand saw",
-              "price": 11.55,
-              "starRating": 3.7,
-              "imageUrl": "http://freepngdownload.com/image/thumb/hand-saw-free-png-image-download-25.png"
-            },
-            {
-              "productId": 10,
-              "productName": "Video Game Controller",
-              "productCode": "GMG-0042",
-              "releaseDate": "October 15, 2015",
-              "description": "Standard two-button video game controller",
-              "price": 35.95,
-              "starRating": 4.6,
-              "imageUrl": "https://cdn2.iconfinder.com/data/icons/game-device-2/512/xone_controller_black_512.png"
-            }
-          
-    ];
+    products: IProduct[] = [];
 
-    constructor(){
-      this.filteredProducts = this.products;
-      this.listFilter = 'cart'; 
+    constructor(private productService: ProductService){
+       
     }
 
     onRatingClicked(message: string):void {
@@ -92,5 +39,11 @@ export class ProductListComponent{
     }
     toggleImage(): void {
       this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
+      
     }
 }
